fe89ba1d3b3ac13f885107989a8fe06a8171c694
Zandra Nicella Alte <62987694+a13zanal@users.noreply.github.com>
2022-03-02 11:17:01 +0100

    Learningprocess och description change
    Change description in README.md and todo-list (learning process) not finish!

--- src/App.js
+++ src/App.js
@@ -1,22 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
+  // Save data
+  var saveData = () => {
+    const whattodo = document.getElementById('new-todo').value;
+    localStorage.setItem('WhatTodo', whattodo);
+    console.log('setItem' + ' ' + whattodo);
+  }
+  //Get data
+  var getData = () => {
+    const data = localStorage.getItem("WhatTodo")
+    console.log('getItem' + ' ' + data);
+  }
+
+  // delete data
+  var deleteData = () => {
+    localStorage.removeItem("WhatTodo")
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h3> LEADERBOARD -1 </h3>
+        <label>Todo-list</label>
+        <div>
+          <input 
+            type='text'
+            id='new-todo'
+            className='input input_lg'
+            name='text'
+            placeholder='Add a to-do'
+            autoComplete='off'
+          />
+          <button onClick={saveData}>Add</button>
+        </div>
+
+        <div>
+          <ul>
+            <li><button onClick={getData}>get</button></li>
+          </ul>
+        </div>
+
+        <div>
+          <p>You have X todo</p>
+          <button onClick={deleteData}>Clear!</button>
+        </div>
+
       </header>
     </div>
   );
