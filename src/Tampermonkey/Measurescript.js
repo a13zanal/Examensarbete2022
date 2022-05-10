b2b6c87589b998bafda4fd01cf571f36cbf4e4a7
Zandra.Alte <zandra.alte@nexergroup.com>
2022-05-10 20:26:48 +0200

    Update Measurescript.js
    moves new Date (). getTime () from the measurement script to respective visualization library

--- src/Tampermonkey/Measurescript.js
+++ src/Tampermonkey/Measurescript.js
@@ -9,57 +9,72 @@
 // @grant        none
 // ==/UserScript==
 
-(function() {
+(function () {
+    var cnt = localStorage.getItem("Counter");
 
-    var cnt = (localStorage.getItem("Counter"));
-
-    window.onload = function(){
-        if(cnt < 101) {
+    window.onload = function () {
+        if (cnt < 1001) {
+            /* When number of measurements + 1 iteration have gone through, send string to getData function and alert "Finish!" */
             startmeasurement();
 
-            var starttime = localStorage.getItem("starttime");
+            /* Get starttime and stoptime from localstorage for comparison between start and end time */
+            var starttime = localStorage.getItem("starttimed3");
+            var stoptime = localStorage.getItem("stoptimed3");
 
-            var stopptime = new Date().getTime();
-            localStorage.setItem("stopptime", stopptime);
+            /* The difference between Stoptime and starttime in Milliseconds, Date.parse() = parses a string representation of a date, and returns the number of milliseconds */
+            var ms = stoptime - starttime;
 
-            var ms = stopptime - starttime;
+            /* If Counter(cnt) is null then set Counter to zero */
+            if (cnt == null) {
+                cnt = 0;
+            }
 
-            if(cnt == null){cnt = 0;}
-
+            /* Set string(str) to the result tab in localstorage */
             var str = localStorage.getItem("result");
 
-            if(cnt == 0){str="data:text/csv;charset=utf-8";}
+            /* If Counter is zero then set data string */
+            if (cnt == 0) {
+                str = "data:text/csv;charset=utf-8";
+            }
 
-            if(str == null){str = "";}
+            /* If string is null set string to empty "" */
+            if (str == null) {
+                str = "";
+            }
 
-            str+= cnt + ", D3, " + ms + "\n";
+            /* Define string measurement */
+            str += cnt + ", D3 50 000, " + ms + "\n";
 
+            /* Copie the string to localstorage */
             localStorage.setItem("result", str);
 
+            /* Increse the Counter every measurement and set Counter in localstorage */
             Math.randSeed = cnt;
             cnt++;
-            localStorage.setItem("Counter",cnt);
+            localStorage.setItem("Counter", cnt);
 
-            setTimeout(redirect, 1000);
+            /* Set a delay before reload the page */
+            setTimeout(redirect, 3000);
         } else {
+            /* Get data and download a datafile when measurement is finish */
             var resultstr = localStorage.getItem("result");
-
-            var download = document.createElement("a");
-            download.setAttribute("href", encodeURI(resultstr));
-            download.setAttribute("download", "d3.csv");
-            download.innerHTML= "Download";
-            document.body.appendChild(download);
-            download.click();
+            // Make anchor and click on download!;
+            var anchor = document.createElement("a");
+            anchor.setAttribute("href", encodeURI(resultstr));
+            anchor.setAttribute("download", "D3_50000.csv");
+            anchor.innerHTML = "Download";
+            document.body.appendChild(anchor);
+            anchor.click();
         }
-    }
+    };
 
-    function startmeasurement(){
-        var start = new Date().getTime();
-        localStorage.setItem("starttime", start);
+    /* Set starttime, save in localstorage och press the chart button */
+    function startmeasurement() {
         document.getElementsByClassName("D3")[0].click();
     }
 
-    function redirect(){
+    /* Reload the page */
+    function redirect() {
         window.location.href = "http://localhost:3000/";
     }
 })();
