5bdfb9e1af17b3128ccf0cbf477862b8179c6064
Zandra.Alte <62987694+a13zanal@users.noreply.github.com>
2022-05-18 15:39:11 +0200

    Update Measurescript.js

--- src/Tampermonkey/Measurescript.js
+++ src/Tampermonkey/Measurescript.js
@@ -14,66 +14,51 @@
 
     window.onload = function () {
         if (cnt < 1001) {
-            /* When number of measurements + 1 iteration have gone through, send string to getData function and alert "Finish!" */
             startmeasurement();
 
-            /* Get starttime and stoptime from localstorage for comparison between start and end time */
-            var starttime = localStorage.getItem("starttimed3");
-            var stoptime = localStorage.getItem("stoptimed3");
+            /* var ms = parseFloat(localStorage.getItem("msRV")); */
+            /* var ms = parseFloat(localStorage.getItem("msRC")); */
+            var ms = parseFloat(localStorage.getItem("msd3"));
 
-            /* The difference between Stoptime and starttime in Milliseconds, Date.parse() = parses a string representation of a date, and returns the number of milliseconds */
-            var ms = stoptime - starttime;
-
-            /* If Counter(cnt) is null then set Counter to zero */
             if (cnt == null) {
                 cnt = 0;
             }
 
-            /* Set string(str) to the result tab in localstorage */
             var str = localStorage.getItem("result");
 
-            /* If Counter is zero then set data string */
             if (cnt == 0) {
                 str = "data:text/csv;charset=utf-8";
             }
 
-            /* If string is null set string to empty "" */
             if (str == null) {
                 str = "";
             }
 
-            /* Define string measurement */
-            str += cnt + ", D3 50 000, " + ms + "\n";
+            str += cnt + ", D3 10000, " + ms + "\n";
 
-            /* Copie the string to localstorage */
             localStorage.setItem("result", str);
 
-            /* Increse the Counter every measurement and set Counter in localstorage */
             Math.randSeed = cnt;
             cnt++;
             localStorage.setItem("Counter", cnt);
 
-            /* Set a delay before reload the page */
-            setTimeout(redirect, 3000);
+            setTimeout(redirect, 3500);
         } else {
-            /* Get data and download a datafile when measurement is finish */
             var resultstr = localStorage.getItem("result");
-            // Make anchor and click on download!;
             var anchor = document.createElement("a");
             anchor.setAttribute("href", encodeURI(resultstr));
-            anchor.setAttribute("download", "D3_50000.csv");
+            anchor.setAttribute("download", "M2_D3_10000.csv");
             anchor.innerHTML = "Download";
             document.body.appendChild(anchor);
             anchor.click();
         }
     };
 
-    /* Set starttime, save in localstorage och press the chart button */
     function startmeasurement() {
+        console.log("Click");
         document.getElementsByClassName("D3")[0].click();
     }
 
-    /* Reload the page */
     function redirect() {
         window.location.href = "http://localhost:3000/";
     }
