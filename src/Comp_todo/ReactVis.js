b12c49e725bcebc10b62c077ebb10644265bccf4
Zandra.Alte <62987694+a13zanal@users.noreply.github.com>
2022-05-18 15:32:49 +0200

    Implement Profiler
    Implementing Profiler API to React-vis and Recharts

--- src/Comp_todo/ReactVis.js
+++ src/Comp_todo/ReactVis.js
@@ -1,4 +1,4 @@
-import React, { useLayoutEffect } from "react";
+import React, { useLayoutEffect, Profiler } from "react";
 import { XYPlot, XAxis, YAxis, VerticalBarSeries } from "react-vis";
 
 const ReactVis = ({ data }) => {
@@ -19,23 +19,68 @@ const ReactVis = ({ data }) => {
         console.log("msReactVis", stoptime - starttime);
     });
 
+    let timeDuration = 0;
+
+    const callback = (
+        id,
+        phase,
+        actualDuration,
+        startTime,
+        baseDuration,
+        commitTime,
+        interactions
+    ) => {
+        timeDuration = timeDuration + actualDuration;
+        console.log(
+            "id " +
+                id +
+                " startTime " +
+                startTime +
+                " actualDuration " +
+                actualDuration +
+                " baseDuration " +
+                baseDuration +
+                " commitTime " +
+                commitTime +
+                " phase " +
+                phase +
+                " interactions " +
+                interactions
+        );
+        console.log("timeDuration ", timeDuration);
+    };
+
+    const getActualDuration = () => {
+        console.log("klar ", timeDuration);
+
+        if (timeDuration > 0) {
+            localStorage.setItem("msRV", timeDuration);
+        } else {
+            console.log("Nooo");
+        }
+    };
+
+    setTimeout(getActualDuration, 3000);
+
     return (
         <div className="charts">
             <h3>React-Vis</h3>
-            <XYPlot
-                xType="ordinal"
-                width={width}
-                height={height}
-                color="#AA4BF5"
-                yDomain={[0, 10000]}
-                margin={{
-                    left: 70,
-                }}
-            >
-                <XAxis />
-                <YAxis />
-                <VerticalBarSeries data={data} />
-            </XYPlot>
+            <Profiler id="ReactVis" onRender={callback}>
+                <XYPlot
+                    xType="ordinal"
+                    width={width}
+                    height={height}
+                    color="#AA4BF5"
+                    yDomain={[0, 10000]}
+                    margin={{
+                        left: 70,
+                    }}
+                >
+                    <XAxis />
+                    <YAxis />
+                    <VerticalBarSeries data={data} />
+                </XYPlot>
+            </Profiler>
         </div>
     );
 };
--- src/Comp_todo/Recharts.js
+++ src/Comp_todo/Recharts.js
@@ -1,45 +1,90 @@
-import React, { useLayoutEffect } from "react";
+import React, { useLayoutEffect, Profiler } from "react";
 import { BarChart, Bar, XAxis, YAxis, ResponsiveContainer } from "recharts";
 
 const Recharts = ({ data }) => {
-    const startRecharts = performance.now();
-    localStorage.setItem("starttimeRecharts", startRecharts);
-    console.log("startRecharts ", startRecharts);
+    // const startRecharts = performance.now();
+    // localStorage.setItem("starttimeRecharts", startRecharts);
+    // console.log("startRecharts ", startRecharts);
 
-    useLayoutEffect(() => {
-        const stopRecharts = performance.now();
-        localStorage.setItem("stoptimeRecharts", stopRecharts);
-        console.log("stopRecharts ", stopRecharts);
+    // useLayoutEffect(() => {
+    //     const stopRecharts = performance.now();
+    //     localStorage.setItem("stoptimeRecharts", stopRecharts);
+    //     console.log("stopRecharts ", stopRecharts);
 
-        const starttime = parseFloat(localStorage.getItem("starttimeRecharts"));
-        const stoptime = parseFloat(localStorage.getItem("stoptimeRecharts"));
-        console.log("msRecharts", stoptime - starttime);
-        console.log(stoptime, localStorage.getItem("stoptimeRecharts"));
-        console.log(starttime);
-    });
+    //     const starttime = parseFloat(localStorage.getItem("starttimeRecharts"));
+    //     const stoptime = parseFloat(localStorage.getItem("stoptimeRecharts"));
+    //     console.log("msRecharts", stoptime - starttime);
+    //     console.log(stoptime, localStorage.getItem("stoptimeRecharts"));
+    //     console.log(starttime);
+    // });
+
+    let timeDuration = 0;
+
+    const callback = (
+        id,
+        phase,
+        actualDuration,
+        startTime,
+        baseDuration,
+        commitTime,
+        interactions
+    ) => {
+        timeDuration = timeDuration + actualDuration;
+        console.log(
+            "id " +
+                id +
+                " startTime " +
+                startTime +
+                " actualDuration " +
+                actualDuration +
+                " baseDuration " +
+                baseDuration +
+                " commitTime " +
+                commitTime +
+                " phase " +
+                phase +
+                " interactions " +
+                interactions
+        );
+        console.log("timeDuration ", timeDuration);
+    };
+
+    const getActualDuration = () => {
+        console.log("klar ", timeDuration);
+
+        if (timeDuration > 0) {
+            localStorage.setItem("msRC", timeDuration);
+        } else {
+            console.log("error");
+        }
+    };
+
+    setTimeout(getActualDuration, 9000);
 
     return (
         <div className="charts">
             <h3>Recharts</h3>
-            <ResponsiveContainer width={800} aspect={2}>
-                <BarChart
-                    data={data}
-                    margin={{
-                        top: 50,
-                        right: 50,
-                        left: 50,
-                        bottom: 50,
-                    }}
-                >
-                    <XAxis dataKey="team" />
-                    <YAxis domain={[0, 10000]} tickCount={11} />
-                    <Bar
-                        dataKey="starpoints"
-                        fill="#AA4BF5"
-                        isAnimationActive={false}
-                    />
-                </BarChart>
-            </ResponsiveContainer>
+            <Profiler id="Recharts" onRender={callback}>
+                <ResponsiveContainer width={800} aspect={2}>
+                    <BarChart
+                        data={data}
+                        margin={{
+                            top: 50,
+                            right: 50,
+                            left: 50,
+                            bottom: 50,
+                        }}
+                    >
+                        <XAxis dataKey="team" />
+                        <YAxis domain={[0, 10000]} tickCount={11} />
+                        <Bar
+                            dataKey="starpoints"
+                            fill="#AA4BF5"
+                            isAnimationActive={false}
+                        />
+                    </BarChart>
+                </ResponsiveContainer>
+            </Profiler>
         </div>
     );
 };
